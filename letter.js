0825881d7a0d2f9000970c277446fe106ed4287f
Jennifer Le <j.le@Jennifers-MacBook-Air.local>
2018-02-19 17:35:35 -0800

    Change letterRender => renderLetter method

--- letter.js
+++ letter.js
@@ -2,7 +2,7 @@ var Letter = function(ltr) {
   this.letter = ltr;
   this.appear = false;
 
-  this.letterRender = function() {
+  this.renderLetter = function() {
     if (this.letter == ' ') { 
       //makes sure that when the function checks if the word is found doesn't read the blank as false.
       this.appear = true;
