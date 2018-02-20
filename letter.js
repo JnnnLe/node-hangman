e0b0a5d6bf23330944a48873de9f51aaa84e2c0f
Jennifer Le <j.le@Jennifers-MacBook-Air.local>
2018-02-19 17:14:44 -0800

    Add letter.js

---
+++ letter.js
@@ -0,0 +1,20 @@
+var Letter = function(ltr) {
+  this.letter = ltr;
+  this.appear = false;
+
+  this.letterRender = function() {
+    if (this.letter == ' ') { 
+      //makes sure that when the function checks if the word is found doesn't read the blank as false.
+      this.appear = true;
+      return '  ';
+    } 
+    if (this.appear === false) { 
+      return ' _ ';
+    } else { 
+      return this.letter;
+    }
+
+  };
+};
+
+module.exports = Letter;
