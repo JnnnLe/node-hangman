c1818e722e494ffa69513a6001946c171188fa26
Jennifer Le <j.le@Jennifers-MacBook-Air.local>
2018-02-19 17:28:04 -0800

    Add getLetters method

---
+++ word.js
@@ -0,0 +1,19 @@
+var Letter = require('./letter.js');
+
+function Word(wrd) {
+  var that = this;
+  this.word = wrd;
+  this.letters = [];
+  this.wordFound = false;
+
+  this.getLetters = function() {
+    //populate the collection above with new Letter objects
+    for (var i = 0; i<that.word.length; i++) {
+      var newLetter = new Letter(that.word[i]);
+      this.letters.push(newLetter);
+    }
+  };
+
+//end of Word constructor  
+}
+module.exports = Word;
